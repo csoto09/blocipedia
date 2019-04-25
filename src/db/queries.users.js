18471b5aa5c6c26746c0cc4f9b9915f56c5db0ce
Carlos J. Soto <csoto09@gmail.com>
2019-04-24 23:17:06 -0400

    cp3 user sign up complete

---
+++ src/db/queries.users.js
@@ -0,0 +1,34 @@
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+
+module.exports = {
+  createUser(newUser, callback){
+    const salt = bcrypt.genSaltSync()
+    const hashedPassword = bcrypt.hashSync(newUser.password, salt)
+
+    return User.create({
+      name: newUser.name,
+      email: newUser.email,
+      password: hashedPassword
+    })
+    .then((user) => {
+      const msg = {
+        to: `${user.email}`,
+        from: 'csoto09@gmail.com',
+        subject: 'Welcome to Blocipedia!',
+        text: `Dear ${user.name},\n\n You successfully created an account at Blocipedia! That makes you the coolest cat on the bloc!`,
+        html: `<p>Dear ${user.name},</p>
+        
+        <p>You successfully created an account at Blocipedia! That makes you the coolest cat on the bloc!</p>`,
+      };
+      sgMail.send(msg);
+      callback(null, user)
+    }).catch((err) => {
+      callback(err)
+    });
+  }
+}
