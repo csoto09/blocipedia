576d223f26baae45159b54ea266d44e5633cbf35
Carlos J. Soto <csoto09@gmail.com>
2019-05-04 09:54:23 -0400

    cp6 - user roles complete

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -15,6 +15,11 @@ module.exports = (sequelize, DataTypes) => {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "0"
     }
   }, {});
   User.associate = function(models) {
